2c71c27702dbfc0125cfeb414524d14770ea85b9
daniel <55977535+danielrostami999@users.noreply.github.com>
2025-07-09 23:30:21 +0330

    Add files via upload

---
+++ Students.java
@@ -0,0 +1,13 @@
+public class Students {
+    String name;
+    int age;
+    double grade;
+    String result;
+    Students(String name, int age, double grade, String result){
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+        this.result = result;
+    }
+    
+}
