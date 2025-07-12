1a8076576ead56241e85554096a2c20dba6128e0
daniel <55977535+danielrostami999@users.noreply.github.com>
2025-07-12 14:35:38 +0330

    Add files via upload

--- Notebook/Main.java
+++ Notebook/Main.java
@@ -1,1 +1,29 @@
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args){
+        
+        while (true) {
+            System.out.println("------------------");
+            System.out.println("1. Add new note: \n" + "2. Show notes: \n" + "3. Remove notes: \n" + "4. Exit:");
+            System.out.print("choose 1 to 4 options:");
+            Scanner input = new Scanner(System.in);
+            int Input = input.nextInt();
+            switch (Input) {
+                case 1:
+                    
+                    break;
+                case 2:
+                    
+                    break;
+                case 3:
+                    
+                    break;
+                case 4:
+                    break;        
+                default:
+                    break;
+            }
+        }
+    }
+}
