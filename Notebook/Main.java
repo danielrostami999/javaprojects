15ac242734f08d1924f18c4794463acac6777ca4
daniel <55977535+danielrostami999@users.noreply.github.com>
2025-07-13 14:31:38 +0330

    Add files via upload

--- Notebook/Main.java
+++ Notebook/Main.java
@@ -1,25 +1,36 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args){
-        
+        Scanner input = new Scanner(System.in);
+        Notebook notebook = new Notebook();
+
         while (true) {
             System.out.println("------------------");
             System.out.println("1. Add new note: \n" + "2. Show notes: \n" + "3. Remove notes: \n" + "4. Exit:");
             System.out.print("choose 1 to 4 options:");
-            Scanner input = new Scanner(System.in);
             int Input = input.nextInt();
+           
             switch (Input) {
                 case 1:
-                    
+                    input.nextLine();
+                    System.out.println("write your note:");
+                    String note = input.nextLine();
+                    notebook.addNote(note);
                     break;
                 case 2:
-                    
+                    notebook.showNotes();
                     break;
                 case 3:
-                    
+                    notebook.showNotes();
+                    System.out.print("Enter note number to remove: ");
+                    int index = input.nextInt();
+                    notebook.RemoveNote(index);
+                    notebook.showNotes();
                     break;
                 case 4:
+                    input.close();
+                    System.exit(0);
                     break;        
                 default:
                     break;
--- Notebook/Notebook.java
+++ Notebook/Notebook.java
@@ -1,3 +1,42 @@
 public class Notebook {
+    final int MAX = 10;
+    String[] notes = new String[MAX];
+    int count = 0;
 
+    public void addNote(String note){
+        if(count < MAX){
+            notes[count] = note;
+            count++;
+            System.out.println("note added!");
+        }else System.out.println("notebook Full!");
+    }
+
+    public void showNotes(){
+        if(count > 0){
+            for(int i = 0 ;i < count; i++){
+                System.out.println("-------------");
+                System.out.println("[" + (i + 1) +"] : " + notes[i]);
+            }
+        }else{
+            System.out.println("note is empty!");
+            return;
+        }
+    }
+    public void RemoveNote(int index){
+        if(count == 0){
+            System.out.println("notes is alredy empty!!!");
+            return;
+        }
+        if (index < 1 || index > count){
+            System.out.println("Invalid note number!\nPlease choose between 1 and " + count);
+            return;
+        }
+        for(int i = index -1; i < count; i++){
+            notes [i] = notes[i+1];
+        }
+        notes[count] = null;
+        count--;
+        System.out.println("Note removed.");
+
+    }
 }
